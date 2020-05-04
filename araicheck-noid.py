e89de557ad9f805c797bbcc414e2f3323e698b94
emojifreak <53028623+emojifreak@users.noreply.github.com>
2020-05-04 09:11:12 +0900

    Check the number of 「☑︎」

--- araicheck-noid.py
+++ araicheck-noid.py
@@ -22,7 +22,7 @@ api = twitter.Api(
 
 today_yday = time.localtime().tm_yday
 for target_user in target_users:
-    tweets = api.GetUserTimeline(screen_name=target_user, include_rts=False, exclude_replies=True) # , count=200
+    tweets = api.GetUserTimeline(screen_name=target_user, include_rts=False, exclude_replies=True, count=200) # , count=200
 
     found = False
     for s in tweets:
@@ -30,8 +30,13 @@ for target_user in target_users:
         tweet_tm = time.localtime(email.utils.mktime_tz(email.utils.parsedate_tz(s.created_at)))
         if arai_string in s.text and ((tweet_tm.tm_hour == 0 and tweet_tm.tm_yday == today_yday) or
                (tweet_tm.tm_hour == 23 and tweet_tm.tm_yday == today_yday-1)):
-            api.PostUpdate("💪偉い🏋️", in_reply_to_status_id=s.id, auto_populate_reply_metadata=True)
-            #print("💪偉い🏋️")
+            if s.text.count('☑') >= 5:
+                api.PostUpdate("💪偉い🏋️", in_reply_to_status_id=s.id, auto_populate_reply_metadata=True)
+                #print("💪偉い🏋️")
+            else:
+                api.PostUpdate("もうひと頑張り😘", in_reply_to_status_id=s.id, auto_populate_reply_metadata=True)
+                #print("もうひと頑張り😘")
+                
             found = True
             break
     if not found:
